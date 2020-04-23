51976868d3cb4cf70ce07c9c8014ae60dc77d93f
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-23 16:30:19 +0200

    initial project

---
+++ ClassLibrary1/BuisnessLogic.cs
@@ -0,0 +1,22 @@
+﻿using DemoLibrary.Utilities;
+using System;
+
+namespace DemoLIbrary
+{
+    public class BuisnessLogic
+    {
+
+        public void ProcessData()
+        {
+            Logger logger = new Logger();
+            DataAccess dataAccess = new DataAccess();
+
+            logger.Log("Starting the processing data.");
+            Console.WriteLine("Processsing the data");
+            dataAccess.LoadData();
+            dataAccess.SaveData("ProcessedInfo");
+            logger.Log("Finished processing of the data");
+        }
+
+    }
+}
---
+++ DependencyInjectionTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using DemoLIbrary;
+using System;
+
+namespace DependencyInjectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuisnessLogic buisnessLogic = new BuisnessLogic();
+
+            buisnessLogic.ProcessData();
+
+            Console.ReadLine();
+        }
+    }
+}
